3f256ef0387e1df83fc69d6bb684b66db12be3b3
Carlos Apaza <apazacarlos94@gmail.com>
2023-02-22 21:58:53 -0300

    archivos js

---
+++ assets/js/index.js
@@ -0,0 +1,15 @@
+let htmlEvents = "";
+for(let event of data.events){
+    
+    htmlEvents += `<div class="row d-flex justify-content-center my-5">
+    <div class="card col-3 me-5 p-3 shadow-lg" style="width: 18rem;">
+        <img src="${event.image}" class="card-img-top" alt="...">
+        <div class="card-body">
+            <h5 class="card-title">${event.name}</h5>
+            <p class="card-text">${event.description}</p>
+            <p class="d-inline float-start">$${event.price}</p><a href="./details"
+                class="btn btn-primary d-inline float-end shadow-lg">Ver más...</a>
+        </div>
+    </div>`
+}
+console.log(htmlEvents);
---
+++ assets/js/pastEvents.js
@@ -0,0 +1,18 @@
+let pastEvents = "";
+
+for (let event of data.events) {
+    let currentDate = new Date(data.currentDate);
+    let eventDate = new Date(event.date);
+    if (currentDate > eventDate) {
+        pastEvents += `<div class="row d-flex justify-content-center my-5">
+            <div class="card col-3 me-5 p-3 shadow-lg" style="width: 18rem;">
+                <img src="${event.image}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${event.name}</h5>
+                    <p class="card-text">${event.description}</p>
+                    <p class="d-inline float-start">$${event.price}</p><a href="./details"
+                        class="btn btn-primary d-inline float-end shadow-lg">Ver más...</a>
+                </div>
+            </div>`
+    }
+}
