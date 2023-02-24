6a2bdc63d94a7e33b6c1521d377c64bc9e7a3fff
Carlos Apaza <apazacarlos94@gmail.com>
2023-02-23 22:17:06 -0300

    JS inyectado

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,17 @@
 let htmlEvents = "";
 for(let event of data.events){
     
-    htmlEvents += `<div class="row d-flex justify-content-center my-5">
-    <div class="card col-3 me-5 p-3 shadow-lg" style="width: 18rem;">
+    htmlEvents += 
+    `<div class="card col-3 me-5 p-3 shadow-lg" style="width: 18rem;">
         <img src="${event.image}" class="card-img-top" alt="...">
         <div class="card-body">
             <h5 class="card-title">${event.name}</h5>
             <p class="card-text">${event.description}</p>
-            <p class="d-inline float-start">$${event.price}</p><a href="./details"
+            <p class="d-inline float-start">$${event.price}</p><a href="./details.html"
                 class="btn btn-primary d-inline float-end shadow-lg">Ver más...</a>
         </div>
-    </div>`
+    </div>`;
 }
-console.log(htmlEvents);
+let cardIndex = document.getElementById('containerBox');
+    cardIndex.innerHTML = htmlEvents;
+
